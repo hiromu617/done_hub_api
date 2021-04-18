2e6e1596bff90f448f9a5cbabbbf289f26ab3db9
hiro <hiromhiro617@gmail.com>
2021-04-18 15:13:04 +0900

    karamuhennkou

---
+++ db/migrate/20210418061044_delete_avatar_column.rb
@@ -0,0 +1,5 @@
+class DeleteAvatarColumn < ActiveRecord::Migration[6.0]
+  def change
+    remove_column :users, :avatar, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_17_065551) do
+ActiveRecord::Schema.define(version: 2021_04_18_061044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,6 @@ ActiveRecord::Schema.define(version: 2021_04_17_065551) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "profile"
-    t.string "avatar"
   end
 
   add_foreign_key "likes", "done_posts"
