e08a9bc011a6f767d4e34a9d87c78f839d2a1a55
hiro <hiromhiro617@gmail.com>
2021-04-18 13:20:13 +0900

    likeを作成

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -1,11 +1,16 @@
 class Api::LikesController < ApplicationController
   def create
     Like.create(like_params)
+    @donePost = DonePost.find(like_params[:done_post_id])
+    render json: @donePost.likes
   end
 
   def destroy
     @like = Like.find_by(done_post_id: params[:done_post_id], user_id: params[:user_id])
     @like.destroy
+
+    @donePost = DonePost.find(params[:done_post_id])
+    render json: @donePost.likes
   end
 
   private
