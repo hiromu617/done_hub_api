bc02285594360b664fc5d57c048d56330e49923b
hiro <hiromhiro617@gmail.com>
2021-04-29 21:05:43 +0900

    created_atを追加

--- app/serializers/notification_serializer.rb
+++ app/serializers/notification_serializer.rb
@@ -1,5 +1,5 @@
 class NotificationSerializer < ActiveModel::Serializer
-  attributes :action, :id
+  attributes :action, :id, :created_at
   belongs_to :done_post
   belongs_to :visiter ,serializer: UserSerializer
 end
