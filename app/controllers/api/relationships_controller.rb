b9a9c3033982716b07a02667cc6d5915e13abc10
hiro <hiromhiro617@gmail.com>
2021-05-01 15:20:10 +0900

    Relationships controllerを修正

--- app/controllers/api/relationships_controller.rb
+++ app/controllers/api/relationships_controller.rb
@@ -1,13 +1,17 @@
 class Api::RelationshipsController < ApplicationController
 
   def create
+    # binding.pry
     @current_user = User.find_by(uid: params[:currentUserUid])
     @user = User.find(params[:id])
-    @current_user.active_notifications.create(
-      visited_id: @user.id,
-      action: "follow"
-    )
-    @current_user.follow(@user)
+    if(@current_user.id != @user.id)
+      @current_user.active_notifications.create(
+        visited_id: @user.id,
+        action: "follow"
+      )
+      @current_user.follow(@user)
+    end
+    
   end
 
   def destroy
