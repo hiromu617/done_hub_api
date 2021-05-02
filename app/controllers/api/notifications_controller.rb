4a5720c0c3367e9ec785119ae34c763a7b84f153
hiro <hiromhiro617@gmail.com>
2021-05-02 12:25:23 +0900

    修正

--- app/controllers/api/notifications_controller.rb
+++ app/controllers/api/notifications_controller.rb
@@ -2,7 +2,7 @@ class Api::NotificationsController < ApplicationController
   def index
       # binding.pry
       @currentUser = User.find_by(uid: params[:uid])
-      @notifications = @currentUser.passive_notifications.page(params[:page]).per(5)
+      @notifications = @currentUser.passive_notifications.page(params[:page]).per(10)
     #@notificationの中でまだ確認していない(indexに一度も遷移していない)通知のみ
       # @notifications.where(checked: false).each do |notification|
       #     notification.update_attributes(checked: true)
