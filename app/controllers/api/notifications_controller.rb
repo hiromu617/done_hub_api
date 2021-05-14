088fcffcc98f5764bf13d33788fdeb23eb4ab01e
hiro <hiromhiro617@gmail.com>
2021-05-14 18:26:32 +0900

    notification

--- app/controllers/api/notifications_controller.rb
+++ app/controllers/api/notifications_controller.rb
@@ -4,6 +4,10 @@ class Api::NotificationsController < ApplicationController
       @currentUser = User.find_by(uid: params[:uid])
       @notifications = @currentUser.passive_notifications.page(params[:page]).per(10)
       render json: @notifications, include: ['visiter', 'done_post' ,'done_post.user','done_post.replys','done_post.likes','done_post.replys.user','done_post.likes.user']
+
+      @notifications.where(checked: false).each do |notification|
+        notification.update_attributes(checked: true)
+      end
   end
 
   def update
